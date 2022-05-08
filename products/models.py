e7d1c0421aa37e7a69799ef6db9d69f5c3c07a27
krzysieq <krzysiek4me@gmail.com>
2022-05-09 00:42:41 +0200

    kolejna zmiana w models

--- products/models.py
+++ products/models.py
@@ -2,11 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Product(models.Model):
-    title       = models.CharField(max_length=120) #max_lenght = required
-    description = models.TextField(blank=True, null=True)
-    price       = models.DecimalField(decimal_places=2, max_digits=10000)
+    title       = models.TextField()
+    description = models.TextField()
+    price       = models.TextField()
     summary     = models.TextField()
 
 
+
+# lekcja 8 - OK
 
 
